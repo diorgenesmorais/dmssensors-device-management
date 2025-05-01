d0d966ea4f60f2a832b79878312a69965714d858
Diorgenes M. Silva <diorgenesmorais@gmail.com>
2025-05-01 19:43:18 -0300

    feat: create generator tsid

---
+++ src/test/java/com/dms/dmssensors/device/management/TSIDTest.java
@@ -0,0 +1,20 @@
+package com.dms.dmssensors.device.management;
+
+import io.hypersistence.tsid.TSID;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+class TSIDTest {
+     @Test
+     void testGenerateTSID() {
+         TSID tsid = IdGenerate.generateTSID();
+
+         System.out.println(String.format("TSID gerado em: %s", tsid.getInstant()));
+
+         Assertions.assertThat(tsid.getInstant())
+                 .isCloseTo(Instant.now(), Assertions.within(1, ChronoUnit.MINUTES));
+     }
+}
